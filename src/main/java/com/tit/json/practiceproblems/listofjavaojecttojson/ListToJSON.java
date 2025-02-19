b4e8313d5112ea88275e57f43dfa5d55b9842a2e
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-02-19 22:15:28 +0530

    [Aditya] Add . Functionality to convert a list of Java objects into a JSON array

---
+++ src/main/java/com/tit/json/practiceproblems/listofjavaojecttojson/ListToJSON.java
@@ -0,0 +1,37 @@
+package com.tit.json.practiceproblems.listofjavaojecttojson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Car{
+    public String brand;
+    public String model;
+    public int year;
+    public double price;
+    public Car(String brand, String model, int year, double price){
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+}
+public class ListToJSON {
+    public static void main(String[] args) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Honda", "Civic", 2023, 25000));
+        cars.add(new Car("Toyota", "Corolla", 2022, 22000));
+        cars.add(new Car("Tesla", "Model 3", 2024, 45000));
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String jsonArray = objectMapper.writeValueAsString(cars);
+            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            System.out.println(jsonArray);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
